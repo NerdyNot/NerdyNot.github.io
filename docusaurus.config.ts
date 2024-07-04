6dcaf6ba5d232b70b5c38072ca8267a8fa1c3d2e
nerdynot <nerdynot92@gmail.com>
2024-07-04 14:24:33 +0900

    번역 테스트

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -4,26 +4,18 @@ import type * as Preset from '@docusaurus/preset-classic';
 
 const config: Config = {
   title: 'NerdyNot Docs',
-  tagline: 'NerdyNot\'s documentation site',
+  tagline: "NerdyNot's documentation site",
   favicon: 'img/favicon.ico',
 
-  // Set the production url of your site here
   url: 'https://nerdynot.github.io',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'nerdynot', // Usually your GitHub org/user name.
-  projectName: 'nerdynot.github.io', // Usually your repo name.
+  organizationName: 'nerdynot',
+  projectName: 'nerdynot.github.io',
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
-  // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
-  // may want to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'ko'],
@@ -34,28 +26,23 @@ const config: Config = {
       'classic',
       {
         docs: {
-          sidebarPath: './sidebars.ts',
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
+          sidebarPath: require.resolve('./sidebars.js'),
           editUrl:
             'https://github.com/NerdyNot/NerdyNot.github.io/tree/main/',
         },
         blog: {
           showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
           editUrl:
             'https://github.com/NerdyNot/NerdyNot.github.io/tree/main/',
         },
         theme: {
-          customCss: './src/css/custom.css',
+          customCss: require.resolve('./src/css/custom.css'),
         },
       } satisfies Preset.Options,
     ],
   ],
 
   themeConfig: {
-    // Replace with your project's social card
     image: 'img/docusaurus-social-card.jpg',
     navbar: {
       title: 'NerdyNot Docs',
@@ -72,13 +59,17 @@ const config: Config = {
         },
         {
           type: 'docSidebar',
-          sidebarId: 'StudySidebar',
+          sidebarId: 'WikiSidebar',
           position: 'left',
-          label: 'Study',
+          label: 'Wiki',
         },
         {
           href: 'https://github.com/NerdyNot/nerdynot.github.io',
           label: 'GitHub',
+          position: 'right',
+        },
+        {
+          type: 'localeDropdown',
           position: 'right',
         },
       ],
@@ -86,32 +77,6 @@ const config: Config = {
     footer: {
       style: 'dark',
       links: [
-        {
-          title: 'Docs',
-          items: [
-            {
-              label: 'Tutorial',
-              to: '/docs/intro',
-            },
-          ],
-        },
-        {
-          title: 'Community',
-          items: [
-            {
-              label: 'Stack Overflow',
-              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-            },
-            {
-              label: 'Discord',
-              href: 'https://discordapp.com/invite/docusaurus',
-            },
-            {
-              label: 'Twitter',
-              href: 'https://twitter.com/docusaurus',
-            },
-          ],
-        },
         {
           title: 'More',
           items: [
@@ -122,7 +87,7 @@ const config: Config = {
           ],
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} NerydNot.`,
+      copyright: `Copyright © ${new Date().getFullYear()} NerdyNot.`,
     },
     prism: {
       theme: prismThemes.github,
--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -1,55 +1,45 @@
 import clsx from 'clsx';
 import Heading from '@theme/Heading';
 import styles from './styles.module.css';
+import Link from '@docusaurus/Link';
 
 type FeatureItem = {
   title: string;
-  Svg: React.ComponentType<React.ComponentProps<'svg'>>;
   description: JSX.Element;
+  permalink: string;
 };
 
 const FeatureList: FeatureItem[] = [
   {
-    title: 'Easy to Use',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    title: 'NerdyOps',
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        This is a documentation page for NerdyOps, a remote management program that utilizes LLM and LangChain.
       </>
     ),
+    permalink: '/docs/NerdyOps/Introduction', 
   },
   {
-    title: 'Focus on What Matters',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+    title: 'Wiki Docs',
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        It's a Wiki page that covers topics related to DevOps and other technical subjects.
       </>
     ),
-  },
-  {
-    title: 'Powered by React',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
-    description: (
-      <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
-      </>
-    ),
+    permalink: '/docs/Wiki/DevOps',
   },
 ];
 
-function Feature({title, Svg, description}: FeatureItem) {
+
+function Feature({ title, description, permalink }: FeatureItem) {
   return (
-    <div className={clsx('col col--4')}>
-      <div className="text--center">
-        <Svg className={styles.featureSvg} role="img" />
-      </div>
+    <div className={clsx('col col--6', styles.featureItem)}>
       <div className="text--center padding-horiz--md">
         <Heading as="h3">{title}</Heading>
         <p>{description}</p>
+        <Link className="button button--secondary button--lg" to={permalink}>
+          Read More
+        </Link>
       </div>
     </div>
   );
